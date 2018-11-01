5d2c11972a3b9e3677273121e93d1970d7908b12
sepanther <sepanta.mohseni@gmail.com>
2018-11-01 18:42:50 -0400

    working website

---
+++ assets/javascript/app.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+
+var topics = ["cat", "dog", "fish"]
+
+
+
+
+//Function to create buttons
+function createButtons() {
+    $("#buttons").empty();
+    for (i=0;i<topics.length;i++) {
+        var newButton = $("<button>")
+        newButton.addClass("gif-button")
+        newButton.text(topics[i])
+        newButton.val(topics[i])
+        $("#buttons").append(newButton)
+        console.log(newButton);
+    }
+}
+
+createButtons();
+
+$(document.body).on("click", ".gif-button", function() {
+    var searchTag = this.value
+    console.log(searchTag)
+    var gifKey = "8pfzJ6HMlxspmR7FHDexfN7c1ywi62NR"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTag + "&api_key=" + gifKey + "&limit=10"
+    $.ajax({
+        url:queryURL,
+        method:"GET"
+    }).then(function(response) {
+        results = response.data
+        console.log(queryURL)
+        console.log(results);
+        for (i=0;i<results.length;i++) {
+            var newDiv = $("<div>")
+            newDiv.addClass("gif-box")
+            newGif = $("<img>")
+            newGif.addClass("gif-image")
+            newGif.attr("src",results[i].images.fixed_height_still.url)
+            newGif.attr("gif-motion", results[i].images.fixed_height.url)
+            newGif.attr("gif-still",results[i].images.fixed_height_still.url)
+            newGif.attr("status", "still")
+            var newRating = $("<div>")
+            newRating.text("Rating: " + results[i].rating.toUpperCase())
+            newDiv.append(newGif)
+            newDiv.append(newRating)
+            $("#gifs-appear-here").prepend(newDiv);
+        }
+        
+    })
+
+})
+
+$(document.body).on("click", ".gif-image", (function() {
+    if ($(this).attr("status") == "still") {
+        $(this).attr("src", $(this).attr("gif-motion"))
+        $(this).attr("status", "motion")
+    }
+    else if ($(this).attr("status") == "motion") {
+        $(this).attr("src", $(this).attr("gif-still"))
+        $(this).attr("status", "still")
+    }
+
+}))
+
+$("#gif-submit").on("click", function() {
+    event.preventDefault();
+    searchTerm = $("#gif-input").val().trim();
+    $("#gif-input").val("");
+    topics.push(searchTerm);
+    createButtons();
+})
+
+
+})
